60038b5a3976357fa235f422b330da2009047d05
Ronak Ajwani <2022.ronak.ajwani@ves.ac.in>
2024-09-13 16:28:35 +0530

    Without Firebase

--- lib/screens/Whiteboard/whiteboard_page.dart
+++ lib/screens/Whiteboard/whiteboard_page.dart
@@ -136,21 +136,11 @@ class _DrawingPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     for (int i = 0; i < drawingPoints.length - 1; i++) {
-      if (drawingPoints[i] != null && drawingPoints[i + 1] != null) {
-        canvas.drawLine(
-          drawingPoints[i].offset,
-          drawingPoints[i + 1].offset,
-          drawingPoints[i].paint,
-        );
-      } else if (drawingPoints[i] != null && drawingPoints[i + 1] == null) {
-        offsetsList.clear();
-        offsetsList.add(drawingPoints[i].offset);
-        canvas.drawPoints(
-          PointMode.points,
-          offsetsList,
-          drawingPoints[i].paint,
-        );
-      }
+      canvas.drawLine(
+        drawingPoints[i].offset,
+        drawingPoints[i + 1].offset,
+        drawingPoints[i].paint,
+      );
     }
   }
 
